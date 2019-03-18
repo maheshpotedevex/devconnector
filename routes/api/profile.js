8b92e673783ee89825a51c1ab758aa615cbf8c52
maheshpote <maheshpote.inmac@gmail.com>
2019-03-18 17:39:26 +0530

    Check current user profile api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -16,4 +16,24 @@ const User = require("../../models/User");
 
 router.get("/test", (req, res) => res.json({ msg: "Profile Works" }));
 
+// @route GET api/profile
+// @desc get Current user profile
+// @access Private
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const errors = {};
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        if (!profile) {
+          errors.noprofile = "There is no profile for this user";
+          return res.status(404).json(errors);
+        }
+        res.json(profile);
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
 module.exports = router;
